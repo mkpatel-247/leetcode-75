eeff8457fbd31640ba6c5c8df47d90f88cf11de3
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2024-09-10 20:44:24 +0530

    leetcode75: Question-1

---
+++ Merge Strings Alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+  public String mergeAlternately(String word1, String word2) {
+    int n = word1.length();
+    int m = word2.length();
+
+    StringBuilder and = new StringBuilder();
+
+    int i=0, j=0;
+    while(i < n || j < m) {
+      if(i < n) {
+        ans.append(word1.charAt(i));
+      }
+      if(j < m) {
+        ans.append(word2.charAt(j));
+      }
+      i++;
+      j++;
+    }
+    return ans.toString();
+  }
+}
