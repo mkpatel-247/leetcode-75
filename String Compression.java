58fc5f7326ef8de8fc5aa729fd75f2e1f0959525
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2024-09-18 22:09:41 +0530

    leetcode75: Question-8

---
+++ String Compression.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder str = new StringBuilder();
+        // char prev = chars[0];
+        int count = 1;
+        str.append(chars[0]);
+        for(int i=1; i<chars.length; i++){
+            System.out.print(i + " ");
+            if(chars[i] == chars[i-1]){
+                count++;
+            } else {
+                if(count > 1) str.append(count);
+                str.append(chars[i]);
+                count = 1;    
+            }
+        }
+        
+        if(count > 1) str.append(count);
+        //Change the existing char array to a new string compression.
+        String compressedStr = str.toString();
+        for(int i=0; i<compressedStr.length(); i++){
+            chars[i] = compressedStr.charAt(i);
+        }
+        return str.toString().length();
+    }
+}
