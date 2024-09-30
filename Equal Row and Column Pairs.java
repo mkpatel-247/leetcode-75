45d11b43dd09a2aa0fd0441e8356daf99db58c9b
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2024-10-01 00:39:23 +0530

    Create Equal Row and Column Pairs.java

---
+++ Equal Row and Column Pairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        HashMap<String, Integer> hmap = new HashMap<>();
+
+        for(int i=0; i<grid.length; i++){
+            String str = "";
+            for(int j=0; j<grid[0].length; j++){
+                str += grid[i][j]+"-";
+            }
+            hmap.put(str, hmap.getOrDefault(str, 0)+1);
+        }
+        int res = 0;
+
+        for(int i=0; i<grid.length; i++){
+            String str = "";
+            for(int j=0; j<grid[0].length; j++){
+                str += grid[j][i]+"-";
+            }
+            int count = hmap.get(str) != null ? hmap.get(str) : 0;
+            if(count > 0){
+                res += count;
+            }
+        }
+        return res;
+    }
+}
