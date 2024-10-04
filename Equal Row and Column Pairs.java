19df134b94820975dbe06e7486887d1796e96212
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2024-10-05 02:36:57 +0530

    leetcode75: Question-21

--- Equal Row and Column Pairs.java
+++ Equal Row and Column Pairs.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i<grid.length; i++){
             String str = "";
             for(int j=0; j<grid[0].length; j++){
-                str += grid[i][j]+"-";
+                str += grid[i][j]+" ";
             }
             hmap.put(str, hmap.getOrDefault(str, 0)+1);
         }
@@ -14,7 +14,7 @@ class Solution {
         for(int i=0; i<grid.length; i++){
             String str = "";
             for(int j=0; j<grid[0].length; j++){
-                str += grid[j][i]+"-";
+                str += grid[j][i]+" ";
             }
             int count = hmap.get(str) != null ? hmap.get(str) : 0;
             if(count > 0){
